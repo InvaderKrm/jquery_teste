e64d73ea9dd73b88e4885ce76d485ac15a8b6602
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2023-09-02 16:17:58 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#cn").click(function(){
+        alert("Como Ciências da Natureza engloba 3 matérias, aprendemos coisas diferentes em cada uma delas. Em Biologia estamos aprendendo sobre Citologia, em química sobre Funções Inorgâmicas e em física sobre Movimento Retilíneo Uniforme.");
+    });
+    $("#ch").click(function(){
+        alert("Em Ciências Humanas estamos aprendendo sobre a distribuição de terras no brasil e os conflitos gerados por causa de diversos fatores historicos.");
+    });
+    $("#mat").click(function(){
+        alert("Em Matemática estamos iniciando o conteúdo de logarítimos e a escala de log.");
+    });
+    $("#ling").click(function(){
+        alert("Como Linguagens engloba 4 matérias, aprendemos coisas diferentes em cada uma delas. Em em Português estamos aprendendo sobre o gênero entrevista, em Inglês sobre o ICT (information and communication technology), em Artes sobre a produção de filmes desde o roteiro até a edição final e em Educação Física sobre diversos tipos de esporte.");
+    });
+    $("#ti").click(function(){
+        alert("Em Técnico Profissional em Informática estamos iniciando o conteúdo de Jquery para que possamos aprofundar as nossas habilidades na criação de sites.");
+    });
+    $("#pp").click(function(){
+        alert("Em Projeto Profissional produzimos o site de uma empresa sustentável com base no conceito ESG (Environmental, Social and Governance).");
+    });
+    $("#pv").click(function(){
+        alert("Em Projeto de Vida se iniciou uma série de mini-aulas onde os alunos ensinam sobre um assunto de tema livre com o objetivo de expandir os conhecimentos daqueles que vão assistir bem como incentivar a pesquisa sobre assuntos diversos.");
+    });
+});
